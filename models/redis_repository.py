7c9bd530f1315c80aa4a1ebbd3a43fba41bb0329
Roberto Athos <robertoathos@bricker.vc>
2023-10-07 10:40:13 -0300

    feat: managed ttl

--- models/redis_repository.py
+++ models/redis_repository.py
@@ -16,5 +16,12 @@ class RedisRepository:
         self.__redis_conn.hset(key, field, value)
 
     def get_hash(self, key: str, field: str) -> any:
-       value = self.__redis_conn.hget(key, field).decode('utf-8')
-       return value
+        value = self.__redis_conn.hget(key, field).decode("utf-8")
+        return value
+
+    def insert_ex(self, key: str, value: any, ex: int) -> None:
+        self.__redis_conn.set(key, value, ex=ex)
+
+    def insert_hash_ex(self, key: str, field: str, value: any, ex: int) -> None:
+        self.__redis_conn.hset(key, field, value)
+        self.__redis_conn.expire(key, ex)
--- run.py
+++ run.py
@@ -1,13 +1,19 @@
+from datetime import datetime
 from models.connection.redis_connection import RedisConnectionHandle
 from models.redis_repository import RedisRepository
 
 redis_conn = RedisConnectionHandle().connect()
 redis = RedisRepository(redis_conn)
 
-redis.insert('Eu sou', 'legal')
-value = redis.get('Eu sou')
+# redis.insert('Eu sou', 'legal')
+# value = redis.get('Eu sou')
 
-redis.insert_hash("meu_hash", "campo_1", "Este e meu valor")
-value = redis.get_hash("meu_hash", "campo_1")
+# redis.insert_hash("meu_hash", "campo_1", "Este e meu valor")
+# value = redis.get_hash("meu_hash", "campo_1")
 
-print(value)
+data_atual = datetime.now()
+data_atual = data_atual.strftime('%Y-%m-%d')
+
+redis.insert_hash_ex(data_atual, "banana", 3.12, 10)
+redis.insert_hash(data_atual, "morango", 5.12)
+redis.insert_hash(data_atual, "uva", 4.12)
